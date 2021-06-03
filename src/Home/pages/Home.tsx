f636ce098bffdef465f042073b60f9b4af6d873a
rednebula <rednebula01018@gmail.com>
2021-06-03 23:58:44 +0900

    [기타] 홈화면 백그라운드 이미지 추가

--- src/Home/pages/Home.tsx
+++ src/Home/pages/Home.tsx
@@ -1,5 +1,15 @@
+import styles from './Home.module.css';
+
 export default function Home() {
   return (
-    <div />
+    <div>
+      <div>
+        <img
+          src={`${process.env.PUBLIC_URL}/img/home-nebula-1.png`}
+          alt="home"
+          className={styles.BackGroundImage}
+        />
+      </div>
+    </div>
   );
 }
